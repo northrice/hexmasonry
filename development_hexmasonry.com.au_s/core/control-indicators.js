4b01f559ced6b1e1977c3d38e7f52dc15a1f5b8c
northrice <164635578+northrice@users.noreply.github.com>
2025-05-13 21:41:01 +1000

    Update control-indicators.js

--- development_hexmasonry.com.au_s/core/control-indicators.js
+++ development_hexmasonry.com.au_s/core/control-indicators.js
@@ -1,4 +1,4 @@
-  function initControlIndicators() {
+  export function initControlIndicators() {
     function easeInOutCubic(t) {
       return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
     }
