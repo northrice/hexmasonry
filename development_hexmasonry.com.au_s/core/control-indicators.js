00f2036419a5540e31c6e133176a73e6fd3ee312
northrice <164635578+northrice@users.noreply.github.com>
2025-05-13 21:54:46 +1000

    Update control-indicators.js

--- development_hexmasonry.com.au_s/core/control-indicators.js
+++ development_hexmasonry.com.au_s/core/control-indicators.js
@@ -1,4 +1,4 @@
-export function initGestureTutorialWithAnimations() {
+export function initControlIndicators() {
   // 1. Inject styles
   const style = document.createElement('style');
   style.textContent = `
